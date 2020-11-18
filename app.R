751895146b347ab0bf268029b70337babaf6d240
viviann <viviann.chan@gmail.com>
2020-11-19 02:12:28 +0800

    update app

--- app.R
+++ app.R
@@ -20,36 +20,44 @@ ui <- fluidPage(
         sidebarLayout(
         
         sidebarPanel(
-            h3("Overview"),
-            "The purpose of this app is to investigate the effect of COVID on US air traffic in 2020.",
-            br(),br(),
-            h3("Further Work"),
-            "Flights dataset contains all aviation traffic around the globe; cases and flights can be 
-            updated until end of 2020 as they become available. An expansion to the current study is to 
-            explore the international trends, also including how travel restrictions affects 
-            international air traffic.",
-            br(),br(),
+            #h3("Overview"),
+            h4(tags$b("The purpose of this app is to investigate the effect of COVID on US air traffic in 2020,
+            or vice versa.")),
+            br(),
             h3("Data sources & Limitations"),
             tags$a(href="https://zenodo.org/record/4266938#.X66h8C-cZQI", "Flights dataset"),
-            " is crowdsourced air traffic data from The OpenSky Network 2020.\n",
-            br(),
+            " is crowdsourced air traffic data from The OpenSky Network 2020. Since this dataset is crowdsourced,
+            the exhausativeness of the list may be incomplete - hence there appears to be very few air traffic for
+            certain states. Flights list is not limited to commerical traffic, as it may include servillance
+            helicopters and private planes. Current dataset has a filter for flights departing and arriving within
+            the US, and does not include flights that takes off and lands at the same airport on the same day. ",
+            br(),br(),
             tags$a(href="https://github.com/CSSEGISandData/COVID-19", 
                    "COVID cases"), 
-            " data is obtained from John Hopkins Center for Systems Science and Enginnering Github page."
+            " data is obtained from John Hopkins Center for Systems Science and Enginnering Github page. ",
+            br(),br(),  
             
+            h3("Further Work"),
+            tags$p("Flights dataset contains all aviation traffic around the globe; cases and flights can be 
+            updated until end of 2020 as they become available. An expansion to the current study is to 
+            explore the international trends, also including how travel restrictions affects 
+            international air traffic."),
+            
+            br(),br(),
+            img(src="US_Regions.gif",width="100%"),
             ),
 
         mainPanel(
             tabsetPanel(
 
                 tabPanel(h3("Overall"), 
-                         
+                         br(),
                          fluidRow(br(),                           
-                             column(6,(
+                             column(4,
                                  dateRangeInput("dates", label = h4("Select Date"),
                                         min = "2020-01-01", max = "2020-10-31",
-                                        start = "2020-02-01", end = "2020-08-31"))),
-                             column(6, br(),br(),
+                                        start = "2020-02-01", end = "2020-08-31")),
+                             column(8, br(),br(),
                                     "data available from 2020/1/1 until 2020/10/31")
                              
                              ),
@@ -60,20 +68,15 @@ ui <- fluidPage(
                          ), 
                 
                 tabPanel(h3("By Region"), 
+                         br(),
                          
-                         # fluidRow(radioButtons("origDest", 
-                         #              label = h4("Flight Region by:"),
-                         #              choices = list("Origin" = "origin.region", 
-                         #                             "Destination" = "dest.region"), 
-                         #              selected = "origin.region")),
-                         #img(src="US_Regions.gif",width="100%"),
                          plotOutput("flightsByRegion"),
                          plotOutput("casesByRegion")
                          
                          ),
                 
                 tabPanel(h3("By State"), 
-                         
+                         br(),
                          fluidRow(
                              radioButtons(
                                  "direction",
@@ -97,7 +100,7 @@ ui <- fluidPage(
                         ),
                 
                 tabPanel(h3("By Airport"), 
-                         
+                         br(),
                          fluidRow(radioButtons("Origin.airportSize",
                                                label = "Origin Airport Size:",
                                                choices = list("Large" = "large_airport",
@@ -140,15 +143,25 @@ server <- function(input, output) {
         flights %>% filter(date >= input$dates[1] & date <= input$dates[2]) %>% 
             group_by(date) %>% count() %>%
             ggplot(., aes(x=date, y=n))+geom_line()+
-            labs(title="flight trends",y="daily flights")
+            labs(title="Flight trends",y="daily flights")+
+            theme(plot.title = element_text(size = rel(2)))
     })
     
     output$allCases <- renderPlot({
-        cases %>% filter(date >= input$dates[1] & date <= input$dates[2]) %>%
-            group_by(date) %>% 
-            summarise(total=sum(cases)) %>% 
-            ggplot(., aes(x=date, y=total))+geom_line()+
-            labs(title="COVID cases", y="cumulative cases")
+        # cases %>% filter(date >= input$dates[1] & date <= input$dates[2]) %>%
+        #     group_by(date) %>% 
+        #     summarise(total=sum(cases)) %>% 
+        #     ggplot(., aes(x=date, y=total))+geom_area(fill="grey")+
+        #     labs(title="COVID cases", y="cumulative cases")+
+        #     theme(plot.title = element_text(size = rel(2)))
+        
+        
+        cases %>% filter(date >= input$dates[1] & date <= input$dates[2]) %>% 
+            group_by(date) %>% summarise(n=sum(new_cases),
+                                               total=sum(cases)) %>% 
+            ggplot(.,aes(x=date))+
+            geom_bar(aes(y=n*50,fill=n),stat="identity") +
+            geom_line(aes(y=total), size=1)
     })
     
     output$flightsByRegion <- renderPlot({
@@ -159,13 +172,15 @@ server <- function(input, output) {
             geom_bar(stat="identity",aes(fill=origin.region))+
             coord_cartesian(xlim = c(1,10)) +
             scale_x_continuous(breaks = 1:10,
-                               labels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct"))
+                               labels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct")) +
+            labs(y="daily flights")
     })
     
     output$casesByRegion <- renderPlot({
         cases %>% group_by(date,region) %>% 
             summarise(total=sum(cases)) %>%
-            ggplot(., aes(x=date, y=total))+geom_line(aes(color=region))
+            ggplot(., aes(x=date, y=total))+geom_line(aes(color=region))+
+            labs(y="cumulative cases")
     })
     
     output$flightsByState <- renderPlot({
@@ -181,15 +196,10 @@ server <- function(input, output) {
     })
     
     output$casesByState <- renderPlot({
-        # cases %>% group_by(date) %>% 
-        #     summarise(total=sum(cases)) %>%
-        #     ggplot(., aes(date, total))+geom_area(fill="grey")
-        # cases %>% filter(state==input$state) %>% 
-        #     ggplot(.,aes(x=date,y=cases)) + geom_line(stat="identity")
         cases %>% filter(state==input$state) %>% arrange(date) %>%
             ggplot(.,aes(x=date)) +
-            #geom_area(aes(y=cases),fill="grey") +
-            geom_bar(stat="identity",aes(y=new_cases,fill=new_cases))
+            geom_bar(stat="identity",aes(y=new_cases,fill=new_cases))+
+            labs(y="new cases")
     })
     
     output$flightsByAirport <- renderPlot({
